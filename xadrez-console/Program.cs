40542bb8ff111e390dfbe7b22e161c05211fce89
Jerry Macedo Castro <leom551@hotmail.com>
2021-04-24 13:12:13 -0300

    created custom exception for boad

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,12 +8,20 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Board b = new Board(8, 8);
-            b.addPiece(new Rook(b, Color.black ), new Position(0,0));
-            b.addPiece(new Rook(b, Color.black ), new Position(1,3));
-            b.addPiece(new King(b, Color.black ), new Position(2,4));
-            Screen.showBoard(b);
-          
+            try
+            {
+                Board b = new Board(8, 8);
+                b.addPiece(new Rook(b, Color.black), new Position(0, 0));
+                b.addPiece(new Rook(b, Color.black), new Position(1, 3));
+                b.addPiece(new King(b, Color.black), new Position(8, 3));
+                Screen.showBoard(b);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
         }
     }
 }
--- xadrez-console/board/Board.cs
+++ xadrez-console/board/Board.cs
@@ -17,11 +17,40 @@
         {
             return pieces[line, column];
         }
-
+        public Piece getPiece(Position pos)
+        {
+            return pieces[pos.line, pos.column];
+        }
         public void addPiece(Piece p, Position pos)
         {
+            if (hasPiece(pos))
+                throw new BoardException("Already exists a piece on this position");
             pieces[pos.line, pos.column] = p;
             p.position = pos;
+        }
+        public bool hasPiece(Position pos)
+        {
+            validatePosition(pos);
+            if (getPiece(pos) != null)
+                return true;
+            return false;
+        }
+
+        public bool isValidPosition(Position pos)
+        {
+            if (pos.line < 0 || pos.line >= qtLines || pos.column < 0 || pos.column >= qtColumns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validatePosition(Position pos)
+        {
+            if (!isValidPosition(pos))
+            {
+                throw new BoardException("invalid position");
+            };
         }
     }
 

90fcc81eefc0801bc2df7b975e3a23a171e64b49
Jerry Macedo Castro <leom551@hotmail.com>
2021-04-24 18:07:43 -0300

    changed black pieces color

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,8 +10,14 @@ namespace xadrez_console
         {
             try
             {
-                ChessPosition chessPosition = new ChessPosition('c', 7);
-                Console.WriteLine(chessPosition.toPosition());
+                Board b = new Board(8, 8);
+                b.addPiece(new King(b, Color.black), new Position(0, 0));
+                b.addPiece(new Rook(b, Color.white), new Position(1, 2));
+                b.addPiece(new Rook(b, Color.black), new Position(3, 4));
+                b.addPiece(new Rook(b, Color.black), new Position(5, 2));
+                b.addPiece(new King(b, Color.white), new Position(5, 3));
+
+                Screen.showBoard(b);
             }
             catch (BoardException e)
             {
--- xadrez-console/Screen.cs
+++ xadrez-console/Screen.cs
@@ -9,19 +9,37 @@ namespace xadrez_console
         {
             for (int i = 0; i < b.qtLines; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < b.qtColumns; j++)
                 {
                     if (b.getPiece(i, j) == null)
                     {
-                        Console.Write("-");
+                        Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(b.getPiece(i, j));
+                        Screen.showPiece(b.getPiece(i, j));
+                        Console.Write(" ");
                     }
 
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void showPiece(Piece p)
+        {
+            if (p.color == Color.white)
+            {
+                Console.Write(p);
+            }
+            else
+            {
+                ConsoleColor auxColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(p);
+                Console.ForegroundColor = auxColor;
             }
         }
     }
